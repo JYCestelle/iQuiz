63d3cb92df272911bce95824969c6dfa89b14cb0
Yicheng Jiang <orange@Yichengs-MacBook-Pro.local>
2018-02-12 11:52:15 -0800

    add setting alert and image.

--- iQuiz/ViewController.swift
+++ iQuiz/ViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func settingBtn(_ sender: UIBarButtonItem) {
+        let alertController = UIAlertController(title: "Settings", message: "Settings go here", preferredStyle: .alert)
+        let defaultAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alertController.addAction(defaultAction)
+        present(alertController, animated: true, completion: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
