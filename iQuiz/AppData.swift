d4a8350e10c9c8229dd5dd6b1a99dafae09eb44c
Yicheng Jiang <orange@D-69-91-232-108.dhcp4.washington.edu>
2018-02-21 00:04:18 -0800

    fetch data sucessfully.

--- iQuiz/AppData.swift
+++ iQuiz/AppData.swift
@@ -16,33 +16,48 @@ class AppData: NSObject {
     open var images : [String] = ["math", "superhero", "science"]
     
     // questions for three categories
-    open var mathQuestions = [
-        "1. What is the remainder when 3^283 is divded by 5 ?",
-        "2. What is 1 + 1?"
-    ]
-    open var mathAnswers = [
-        ["0", "1", "2", "3"],
-        ["2", "4", "6", "8"]
-    ]
-    open var correctMA = ["2", "2"]
+//    open var mathQuestions = [
+//        "1. What is the remainder when 3^283 is divded by 5 ?",
+//        "2. What is 1 + 1?"
+//    ]
+    open var mathQuestions: [String] = []
     
-    open var shieldQuestions = [
-        "1. When was Iron Man released?",
-        "2. How many primary color(s) does the outfit of Captain America have?"
-    ]
-    open var shieldAnswers = [
-        ["2005", "2007", "2008", "2010"],
-        ["3", "5", "4", "6"]
-    ]
-    open var correctShieldA = ["2008", "4"]
+//    open var mathAnswers = [
+//        ["0", "1", "2", "3"],
+//        ["2", "4", "6", "8"]
+//    ]
+    open var mathAnswers: [[String]] = []
     
-    open var scienceQuestions = [
-        "1. What is chemical formula for water?"
-    ]
-    open var scienceAnswers = [
-        ["HO2", "H2O", "HO", "HHHHHHHHH"]
-    ]
-    open var correctSA = ["H2O"]
+//    open var correctMA = ["2", "2"]
+    open var correctMA: [String] = []
+    
+//    open var shieldQuestions = [
+//        "1. When was Iron Man released?",
+//        "2. How many primary color(s) does the outfit of Captain America have?"
+//    ]
+    open var shieldQuestions: [String] = []
+    
+//    open var shieldAnswers = [
+//        ["2005", "2007", "2008", "2010"],
+//        ["3", "5", "4", "6"]
+//    ]
+    open var shieldAnswers: [[String]] = []
+    
+//    open var correctShieldA = ["2008", "4"]
+    open var correctShieldA: [String] = []
+    
+//    open var scienceQuestions = [
+//        "1. What is chemical formula for water?"
+//    ]
+    open var scienceQuestions: [String] = []
+    
+//    open var scienceAnswers = [
+//        ["HO2", "H2O", "HO", "HHHHHHHHH"]
+//    ]
+    open var scienceAnswers: [[String]] = []
+    
+//    open var correctSA = ["H2O"]
+    open var correctSA: [String] = []
     
     func getQuizzes() -> [String] {
         return categories
--- iQuiz/ViewController.swift
+++ iQuiz/ViewController.swift
@@ -11,6 +11,11 @@ import UIKit
 var myIndex = 0
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     var appdata = AppData.shared
+    var defaultUrl: String = "https://tednewardsandbox.site44.com/questions.json"
+    var refreshControl = UIRefreshControl()
+    
+    var timer: Timer!
+    var interval = 30 // in sec
 
     
     @IBOutlet weak var tableView: UITableView!
@@ -51,9 +56,82 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         super.viewDidLoad()
         tableView.dataSource = self
         tableView.delegate = self
+        
+        self.refreshControl.attributedTitle = NSAttributedString(string: "Pull to refresh")
+//        self.refreshControl.addTarget(self, action: #selector(ViewController.handleRefresh(_:)), for: UIControlEvents.valueChanged)
+//        self.categoryTableView.addSubview(refreshControl)
+//
+//        fetchData(defaultUrl)
+//        categoryTableView.dataSource = self
+//        categoryTableView.delegate = self
+//        if timer != nil {
+//            timer.invalidate()
+//        }
+//        startTimer()
     // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    func getQuizData(_ url: String ) {
+        let url = URL(string: url)
+        if url != nil {
+            URLSession.shared.dataTask(with: url!) { (data, response, error) in
+                if error != nil {
+                    let alertController = UIAlertController(title: "Error", message: "Fetching quiz data error. Using locally stored quiz data.", preferredStyle: .alert)
+                    alertController.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+                    self.present(alertController, animated: true, completion: nil)
+                } else { // fetch online data if possible
+                    if let httpStatus = response as? HTTPURLResponse, httpStatus.statusCode != 200 { // request error
+                        let alertController = UIAlertController(title: "Error", message: "Fail to fetching quiz data. Request error.", preferredStyle: .alert)
+                        alertController.addAction(UIAlertAction(title: "Retry", style: .default, handler: nil))
+                        self.present(alertController, animated: true, completion: nil)
+                    } else {
+                        let json = try! JSONSerialization.jsonObject(with: data!, options: .allowFragments) as? [Dictionary<String, AnyObject>]
+                        UserDefaults.standard.setValue(json, forKey: "jsonData")
+                        //UIApplication.shared.quizQuestionRepository.deserializaData()
+                    }
+                }
+                // update UI
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                    self.refreshControl.endRefreshing()
+                }
+                }.resume()
+        }
+    }
+    
+    func parseQuizData(){
+        if let jsonData = UserDefaults.standard.array(forKey: "jsonData") {
+            let json = jsonData as! [Dictionary<String, AnyObject>]
+            //var idx = 0
+            for item in json {
+                self.appdata.categories.append(item["titles"]as! String);
+                self.appdata.descriptions.append(item["desc"]as! String);
+                
+                let questionsData = (item["questions"] as! NSArray) as Array
+                for questionData in questionsData {
+                    var question = questionData as! Dictionary<String, Any>
+                    let integer = Int(question["answer"] as! String)!
+                    var answerArray = question["answers"]as! [String]
+                    let string = answerArray[integer]
+                    if item["titles"]as! String == "Science" {
+                        self.appdata.mathQuestions.append(question["text"] as! String)
+                        self.appdata.mathAnswers.append(question["answers"]as! [String])
 
+                        self.appdata.correctMA.append(string)
+                    }else if item["titles"]as! String == "Marvel Super Heroes"{
+                        self.appdata.shieldQuestions.append(question["text"] as! String)
+                        self.appdata.shieldAnswers.append(question["answers"]as! [String])
+                        self.appdata.correctShieldA.append(string)
+                    }else{
+                        self.appdata.shieldQuestions.append(question["text"] as! String)
+                        self.appdata.shieldAnswers.append(question["answers"]as! [String])
+                        self.appdata.correctShieldA.append(string)
+                    }
+                }
+            }
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
